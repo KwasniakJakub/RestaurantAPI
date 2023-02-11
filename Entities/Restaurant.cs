a3c1a4733b03d8ed063f045c3aebe940df9307e5
KwasniakJakub <jakub.kwasniak@wp.pl>
2023-02-11 16:56:57 +0100

    Authentication - Table with Roles and Users

--- Entities/Restaurant.cs
+++ Entities/Restaurant.cs
@@ -7,11 +7,11 @@ namespace RestaurantAPI.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string  Description { get; set; }
+        public string Description { get; set; }
         public string Category { get; set; }
         public bool HasDelivery { get; set; }
         public string ContactEmail { get; set; }
-        public string  ContactNumber { get; set; }
+        public string ContactNumber { get; set; }
 
         public int AdressId { get; set; }
         public virtual Address Address { get; set; }
--- Entities/RestaurantDbContext.cs
+++ Entities/RestaurantDbContext.cs
@@ -12,32 +12,47 @@ namespace RestaurantAPI.Entities
 
     public class RestaurantDbContext : DbContext
     {
-        private string _connectionString = 
+        private string _connectionString =
             "Server=JAKUB-PC;Database=RestaurantDb;Trusted_connection=True;";
+
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<Address> Adresses { get; set; }
         public DbSet<Dish> Dishes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         //Cechy dla konkretnych kolumn
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<User>()
+                .Property(u => u.Email)
+                .IsRequired();
+
+            modelBuilder.Entity<Role>()
+                .Property(r => r.Name)
+                .IsRequired();
+
             modelBuilder.Entity<Restaurant>()
                 .Property(r => r.Name)
                 .IsRequired()
                 .HasMaxLength(25);
+
             modelBuilder.Entity<Dish>()
-               .Property(d => d.Name)
-               .IsRequired();
+                .Property(d => d.Name)
+                .IsRequired();
+
             modelBuilder.Entity<Address>()
                 .Property(a => a.City)
                 .IsRequired()
                 .HasMaxLength(50);
+
             modelBuilder.Entity<Address>()
                 .Property(a => a.Street)
                 .IsRequired()
                 .HasMaxLength(50);
 
         }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(_connectionString);
