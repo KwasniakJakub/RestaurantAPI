f557ce96c8d9d287363ca9708a59706787effbb0
KwasniakJakub <jakub.kwasniak@wp.pl>
2023-02-24 20:39:34 +0100

    Uploading Files To The Server

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 
@@ -27,6 +28,24 @@ namespace RestaurantAPI.Controllers
             var fileContents = System.IO.File.ReadAllBytes(filePath);
 
             return File(fileContents,contentType ,fileName);
+        }
+
+        [HttpPost]
+        public ActionResult Upload([FromForm] IFormFile file)
+            {
+            if(file != null && file.Length > 0)
+                
+            {
+                var rootPath = Directory.GetCurrentDirectory();
+                var fileName = file.FileName;
+                var fullPath = $"{rootPath}/PrivateFiles/{fileName}";
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return Ok();
+            }
+            return BadRequest();
         }
     }
 }
